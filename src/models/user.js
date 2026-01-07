79489164ac37674ad93804411abd15784c467aab
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2026-01-07 16:00:54 +0530

    add data validation

--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,49 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:30
+
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
     },
     password:{
-        type:String
+        type:String,
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("user is not valid")
+            }
+        }
     },
-});
+    about:{
+        type:String,
+        default:"this is about of user"
+    },
+    Skills:{
+        type:[String]
+    }
+},
+{
+    timestamps:true
+}
+);
 
 const User = mongoose.model("User",userSchema);
 module.exports = User;
