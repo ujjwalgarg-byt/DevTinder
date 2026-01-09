086ce20df1bed681f2e94d9f04b39d9f3244647f
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2026-01-09 19:07:32 +0530

    middleware for authentication

---
+++ src/middlewares/auth.js
@@ -0,0 +1,27 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const userAuth = async(req,res,next)=>{
+     try{
+        //read the token
+        const cookies = req.cookies;
+        const {token} = cookies;
+        if(!token){
+                throw new Error("Invalid Token");
+        } 
+        //validate my token 
+        const decodedValue = await jwt.verify(token ,"Dev@Tinder$123" );
+        const {_id} = decodedValue;
+        //find the user
+        const user = await User.findById(_id);
+        if(!user){
+                throw new Error("User does not Exists");
+        }    
+        req.user = user;//for usind user in next request handler
+        next();
+        }catch(err){
+            res.status(400).send("Error : " + err.message);
+        }
+
+};
+module.exports = {userAuth};
