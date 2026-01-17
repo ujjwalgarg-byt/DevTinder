0cd4bd5136ca15c230bdf273c994904c01e691b5
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2026-01-17 18:56:26 +0530

    add origin of ui

--- src/app.js
+++ src/app.js
@@ -13,7 +13,10 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:" http://localhost:5173",
+    credentials:true
+}));
 app.use(express.json());//middleware for converting json data(daynamic data which will passed by api) into js object
 app.use(cookieParser());//middleware for reading cookies
 
